8099777849a55d929512dd2d3839a1e9d302f25f
jnk5y <johnkyrus@gmail.com>
2017-08-04 10:14:10 -0400

    Update and rename pi_garage_alert_config.py to pi_garage_manager_config.py

--- etc/pi_garage_manager_config.py
+++ etc/pi_garage_manager_config.py
@@ -4,35 +4,34 @@
 # Global settings
 ##############################################################################
 
-# Describes all the garage door being monitored
+# Describes the garage door being monitored
 # time is in seconds
 # start and end hours can be [0-23] - will alert only within those hours
-GARAGE_DOOR =  {
-    'pin': 15,
-    'name': "Garage Door",
-    'alerts': [
-        {
-            'state': 'open',
-            'time': 300,
-            'start': 23,
-            'end': 7,
-            'recipients': [ 'ifttt:garage_event' ]
-        },
-        {
-            'state': 'open',
-            'time': 7200,
-            'start': 0,
-            'end': 23,
-            'recipients': [ 'ifttt:garage_event' ]
-        }
-    ]
-}
+PIN = 15,
+NAME = "Garage Door",
+ALERTS = [
+    {
+        'state': 'open',
+        'time': 300,
+        'start': 23,
+        'end': 7,
+        'recipients': [ 'ifttt:garage_event' ]
+    },
+    {
+        'state': 'open',
+        'time': 7200,
+        'start': 0,
+        'end': 23,
+        'recipients': [ 'ifttt:garage_event' ]
+    }
+]
+
 ##############################################################################
 # Home and away setting. Set it to away with the app when you are on vacation
 # to trigger an alert immediately if the garage door is open.
 # Default setting on startup is home
 ##############################################################################
-HOMEAWAY = ''
+HOMEAWAY = 'home'
 
 # All messages will be logged to stdout and this file
 LOG_FILENAME = "/var/log/pi_garage_alert.log"
