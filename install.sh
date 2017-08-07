b70070294772ce9bdf3f7b3f06052c71005a7ab4
John Kyrus <johnkyrus@gmail.com>
2017-08-07 11:16:06 -0400

    updated the init.d script from an online template and added line to the install script to load the init.d script to rc.d level 3

--- install.sh
+++ install.sh
@@ -8,4 +8,5 @@ cp bin/pi_garage_manager.py /usr/local/sbin/
 cp /usr/local/etc/pi_arage_manager_config.py /usr/local/etc/pi_arage_manager_config.old
 cp etc/pi_garage_manager_config.py /usr/local/etc/
 cp init.d/pi_garage_manager /etc/init.d/
+update-rc.d pi_garage_manager defaults 3
 chown $1 /usr/local/etc/pi_garage_manager_config.py
