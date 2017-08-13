f373d71fa6678d4a3a863c4835589a6c3f59dbb5
jnk5y <johnkyrus@gmail.com>
2017-08-13 14:09:05 -0400

    Update garage_trigger.py

--- bin/garage_trigger.py
+++ bin/garage_trigger.py
@@ -1,17 +1,15 @@
 #!/usr/bin/env python
 import sys
 import os
-from multiprocessing.connection import Client
+from socket
 
 if len(sys.argv) < 2:
     print "You must pass a parameter. (open, close, trigger, away, home, state)"
 
 else:
-#    host = os.getenv('GARAGE_ALERT_HOST', 'localhost')
-#    port = os.getenv('GARAGE_ALERT_PORT', 6000)
-#    address = (host, int(port))
-    address = ('localhost', 6000)
-    conn = Client(address, authkey='secret password')
-    conn.send_bytes(sys.argv[1])
-    print conn.recv_bytes()
-    conn.close()
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    sock.connect(("192.168.86.9", 6000))
+    sock.sendall(sys.argv[1])
+    result = sock.recv(1024)
+    print result
+    sock.close()
