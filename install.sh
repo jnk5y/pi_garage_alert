e8c98b460289187ba9a1becbdd25c94a8878dff6
John Kyrus <johnkyrus@gmail.com>
2017-09-15 10:29:52 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -7,5 +7,5 @@ fi
 cp bin/pi_garage_manager.py /usr/local/sbin/
 cp etc/pi_garage_manager_config.py /usr/local/etc/
 cp init.d/pi_garage_manager /etc/init.d/
-update-rc.d pi_garage_manager defaults 3
+update-rc.d pi_garage_manager defaults
 chown $1 /usr/local/etc/pi_garage_manager_config.py
