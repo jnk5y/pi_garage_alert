f4fb8a490c17fe5a78948bd137ecd87cd7386ebd
John Kyrus <johnkyrus@gmail.com>
2017-09-27 15:32:54 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,10 +1,6 @@
 #!/bin/bash
-if [[ $# -eq 0 ]] ; then
-    echo 'please pass your username'
-    exit 0
-fi
 
-cp bin/pi_garage_manager.py /usr/local/sbin/
-cp etc/pi_garage_manager_config.py /usr/local/etc/
-cp init.d/pi_garage_manager /etc/init.d/
+cp pi_garage_manager.py /usr/local/sbin/
+cp pi_garage_manager_config.py /usr/local/etc/
+cp pi_garage_manager /etc/init.d/
 update-rc.d pi_garage_manager defaults
